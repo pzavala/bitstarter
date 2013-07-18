3e543a726807cb103150e9f7fc4253db09e99ad5
Ubuntu <ubuntu@ip-172-31-16-90.us-west-2.compute.internal>
2013-07-18 02:22:45 +0000

    changed grader js for restler

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -34,6 +35,15 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(url) {
+    var res = restler.get(url);
+    if (res instanceof Error) {
+    	sys.puts('Error: ' + res.message);
+    } else {
+    	return res.toString();
+  }
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +75,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'Url Path', clone(assertUrlExists))
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
